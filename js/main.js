9511b9ee0655295ee1817961bbf1da0c901e9e74
Loren <lorenharris2@gmail.com>
2015-07-16 12:11:23 -0700

    commit

--- js/main.js
+++ js/main.js
@@ -10,8 +10,19 @@ function open_window(){
 }
 
 $(function (){
+	$("#iframe").on("load", function(event) {
+		$("#iframe").contentWindow.postMessage({navigator: navigator}, base_url);
+	}, false);
+	$(window).on("message", function(event) {
+		if (event.origin == base_url && event.data.action) {
+			if (event.data.action == "alert") {
+				alert(event.data.data);
+			}
+		}
+	}, false);
 	if (has_internet){
-		open_window();
+		$("#iframe").attr("src", base_url);
+		//open_window();
 	} else {
 		$("body").html("This app requires internet to function.");
 	}
